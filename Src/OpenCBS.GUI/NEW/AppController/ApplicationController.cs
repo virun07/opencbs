6fac639c621260ef4b23c953acedeb5193aa2c28
Pavel Bastov <pbastov@gmail.com>
2013-10-14 11:39:03 +0600

    Add ApplicationController.

---
+++ Src/OpenCBS.GUI/NEW/AppController/ApplicationController.cs
@@ -0,0 +1,41 @@
+﻿// Copyright © 2013 Open Octopus Ltd.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// 
+// Website: http://www.opencbs.com
+// Contact: contact@opencbs.com
+
+using StructureMap;
+
+namespace OpenCBS.GUI.NEW.AppController
+{
+    public class ApplicationController : IApplicationController
+    {
+        private readonly IContainer _container;
+
+        public ApplicationController(IContainer container)
+        {
+            _container = container;
+            _container.Inject<IApplicationController>(this);
+        }
+
+        public void Execute<T>(T commandData)
+        {
+            var command = _container.TryGetInstance<ICommand<T>>();
+            if (command != null)
+                command.Execute(commandData);
+        }
+    }
+}
