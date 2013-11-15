e81a2c54d07f9a7005863811e20c63b82e66fa9f
Pavel Bastov <pbastov@gmail.com>
2013-11-16 01:14:06 +0600

    Cover MainPresenter with more unit tests.

--- Src/OpenCBS.UnitTest/Presenter/MainPresenterTest.cs
+++ Src/OpenCBS.UnitTest/Presenter/MainPresenterTest.cs
@@ -17,30 +17,111 @@
 // Website: http://www.opencbs.com
 // Contact: contact@opencbs.com
 
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using NSubstitute;
 using NUnit.Framework;
+using OpenCBS.DataContract.CommandData;
 using OpenCBS.Interface;
 using OpenCBS.Interface.Service;
 using OpenCBS.Interface.View;
 using OpenCBS.Presenter;
 
+// ReSharper disable InconsistentNaming
 namespace OpenCBS.UnitTest.Presenter
 {
     [TestFixture]
     public class MainPresenterTest
     {
-        [Test]
-        public void Run()
+        private IMainView _mainView;
+        private IApplicationController _appController;
+        private IAuthService _authService;
+        private MainPresenter _presenter;
+
+        [SetUp]
+        public void Setup()
         {
-            var mainView = Substitute.For<IMainView>();
-            var appController = Substitute.For<IApplicationController>();
-            var authService = Substitute.For<IAuthService>();
+            _mainView = Substitute.For<IMainView>();
+            _appController = Substitute.For<IApplicationController>();
+            _authService = Substitute.For<IAuthService>();
+            _presenter = new MainPresenter(_mainView, _appController, _authService);
+        }
 
-            var presenter = new MainPresenter(mainView, appController, authService);
-            presenter.Run();
+        [Test]
+        public void Run_CallsAttachAndRunOnView()
+        {
+            _presenter.Run();
+            _mainView.Received().Attach(_presenter);
+            _mainView.Received().Run();
+        }
 
-            mainView.Received().Attach(presenter);
-            mainView.Received().Run();
+        [Test]
+        public void Run_ChecksRolePermissions()
+        {
+            _presenter.Run();
+            var permissions = new List<string> { "Role.View", "Role.Add", "Role.Edit", "Role.Delete" }.AsReadOnly();
+            _authService.Received().CanAny(Arg.Is<ReadOnlyCollection<string>>(actualPermissions => permissions.SequenceEqual(actualPermissions)));
+        }
+
+        [Test]
+        public void Run_ChecksUserPermissions()
+        {
+            _presenter.Run();
+            var permissions = new List<string> { "User.View", "User.Add", "User.Edit", "User.Delete" }.AsReadOnly();
+            _authService.Received().CanAny(Arg.Is<ReadOnlyCollection<string>>(actualPermissions => permissions.SequenceEqual(actualPermissions)));
+        }
+
+        [Test]
+        public void Run_ChecksEntryFeePermissions()
+        {
+            _presenter.Run();
+            var permissions = new List<string> { "EntryFee.View", "EntryFee.Add", "EntryFee.Edit", "EntryFee.Delete" }.AsReadOnly();
+            _authService.Received().CanAny(Arg.Is<ReadOnlyCollection<string>>(actualPermissions => permissions.SequenceEqual(actualPermissions)));
+        }
+
+        [Test]
+        public void Run_ChecksLoanProductPermissions()
+        {
+            _presenter.Run();
+            var permissions = new List<string> { "LoanProduct.View", "LoanProduct.Add", "LoanProduct.Edit", "LoanProduct.Delete" }.AsReadOnly();
+            _authService.Received().CanAny(Arg.Is<ReadOnlyCollection<string>>(actualPermissions => permissions.SequenceEqual(actualPermissions)));
+        }
+
+        [Test]
+        public void ShowRoles_ExecutesProperCommand()
+        {
+            _presenter.ShowRoles();
+            _appController.Received().Execute(Arg.Any<ShowRolesData>());
+        }
+
+        [Test]
+        public void ShowUsers_ExecutesProperCommand()
+        {
+            _presenter.ShowUsers();
+            _appController.Received().Execute(Arg.Any<ShowUsersData>());
+        }
+
+        [Test]
+        public void ShowLoanProducts_ExecutesProperCommand()
+        {
+            _presenter.ShowLoanProducts();
+            _appController.Received().Execute(Arg.Any<ShowLoanProductsData>());
+        }
+
+        [Test]
+        public void ShowEntryFees_ExecutesProperCommand()
+        {
+            _presenter.ShowEntryFees();
+            _appController.Received().Execute(Arg.Any<ShowEntryFeesData>());
+        }
+
+        [Test]
+        public void ChangeLanguage_ExecutesProperCommand()
+        {
+            _presenter.ChangeLanguage("en-US");
+            _appController.Received().Execute(Arg.Is<ChangeLanguageData>(data => data.Name == "en-US"));
         }
     }
 }
+// ReSharper restore InconsistentNaming
