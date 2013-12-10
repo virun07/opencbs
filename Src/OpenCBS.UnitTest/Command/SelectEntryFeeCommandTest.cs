54fac684048a607be26224470684d2edd0485e11
Pavel Bastov <pbastov@gmail.com>
2013-12-10 11:33:41 +0600

    Add SelectEntryFeeCommandTest.

---
+++ Src/OpenCBS.UnitTest/Command/SelectEntryFeeCommandTest.cs
@@ -0,0 +1,64 @@
+﻿// Copyright © 2013 Open Octopus Ltd.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// 
+// Website: http://www.opencbs.com
+// Contact: contact@opencbs.com
+
+using NSubstitute;
+using NUnit.Framework;
+using OpenCBS.DataContract;
+using OpenCBS.DataContract.AppEvent;
+using OpenCBS.DataContract.CommandData;
+using OpenCBS.Interface;
+using OpenCBS.Interface.Presenter;
+using OpenCBS.Service.Command;
+
+// ReSharper disable InconsistentNaming
+namespace OpenCBS.UnitTest.Command
+{
+    [TestFixture]
+    public class SelectEntryFeeCommandTest
+    {
+        private ISelectEntryFeePresenter _presenter;
+        private IApplicationController _appController;
+        private SelectEntryFeeCommand _command;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _presenter = Substitute.For<ISelectEntryFeePresenter>();
+            _appController = Substitute.For<IApplicationController>();
+            _command = new SelectEntryFeeCommand(_presenter, _appController);
+        }
+
+        [Test]
+        public void Execute_CommandResultIsNotOk_DoesNotSelectEntryFee()
+        {
+            _presenter.Get().Returns(new Result<int>(CommandResult.Cancel, 0));
+            _command.Execute(new SelectEntryFeeData());
+            _appController.DidNotReceive().Raise(Arg.Any<EntryFeeSelectedEvent>());
+        }
+
+        [Test]
+        public void Execute_CommandResultIsOk_SelectsEntryFee()
+        {
+            _presenter.Get().Returns(new Result<int>(CommandResult.Ok, 1));
+            _command.Execute(new SelectEntryFeeData());
+            _appController.Received().Raise(Arg.Is<EntryFeeSelectedEvent>(data => data.Id == 1));
+        }
+    }
+}
+// ReSharper restore InconsistentNaming
