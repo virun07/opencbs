2dfbcdedcb1676ccd6eaeebbdeff2aaa89b6abea
Kubanychbek <kuban_aa@mail.ru>
2014-03-14 11:59:27 +0600

    Add repayment service

---
+++ Src/OpenCBS.ArchitectureV2/Interface/Service/IRepaymentService.cs
@@ -0,0 +1,12 @@
+﻿using OpenCBS.ArchitectureV2.Service;
+using OpenCBS.CoreDomain.Contracts.Loans;
+
+namespace OpenCBS.ArchitectureV2.Interface.Service
+{
+    public interface IRepaymentService
+    {
+        RepaymentConfiguration Repay(RepaymentConfiguration configuration);
+
+        Loan RepayAndSave(RepaymentConfiguration configuration);
+    }
+}
---
+++ Src/OpenCBS.ArchitectureV2/Service/RepaymentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenCBS.CoreDomain.Accounting;
+using OpenCBS.CoreDomain.Contracts.Loans;
+
+namespace OpenCBS.ArchitectureV2.Service
+{
+    public class RepaymentConfiguration
+    {
+        public Loan Loan { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Principal { get; set; }
+        public decimal Interest { get; set; }
+        public decimal Penalty { get; set; }
+        public decimal Commission { get; set; }
+        public DateTime Date { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public string Comment { get; set; }
+        public string ScriptName { get; set; }
+    }
+}
