a07a6fc703825a754d50679224e22d47bd0ef3f5
Nakai uulu Bek <bek15071991@mail.ru>
2014-03-21 11:32:33 +0600

    After rescheduling return interest rate. Corrected commit

--- Sql/Database_Update_v14.3.0.0_to_v14.4.0.0.sql
+++ Sql/Database_Update_v14.3.0.0_to_v14.4.0.0.sql
@@ -1,3 +1,9 @@
+IF  NOT EXISTS(SELECT *  FROM  INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'ReschedulingOfALoanEvents'  AND COLUMN_NAME = 'previous_interest_rate') 
+BEGIN
+ALTER TABLE  ReschedulingOfALoanEvents ADD  previous_interest_rate DECIMAL(25,15) NOT NULL DEFAULT 0
+END
+GO
+
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v14.4.0.0'
 WHERE   [name] = 'VERSION'
